250ecc138cb6e2564ab250db43f3f8baf7703cac
chrialge <chrialge@gmail.com>
2024-03-13 17:04:16 +0100

    add milestone 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,24 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      done: false,
+      text: '',
+      arrayText: [
+        'Giacomo',
+        'Francesco',
+        'lorenzo',
+        'Ricardo'
+      ],
+    }
+  },
+  methods: {
+
+    lineThrough(index){
+        if(this.arrayText[index]){
+          this.done = true  
+        }
+        
     }
   }
 }).mount('#app')
